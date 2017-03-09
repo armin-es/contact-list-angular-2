f9387797e1f8e73194fb4175fa84b732078d3723
Armin Eslami <armin.eslami@gmail.com>
2017-03-08 23:35:56 -0500

    feat: Display first contact in the app
    add 'title' and 'contact' properties to 'AppComponent' class. Update template in '@Component' decoration with data binding to new properties.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{contact}} details!</h2>
+        `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+    title = 'Contact List';
+    contact = 'Armin Eslami';
+}
