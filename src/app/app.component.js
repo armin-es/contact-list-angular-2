80b455b876e37201361b131bc03a9246248a03a8
Armin Eslami <armin.eslami@gmail.com>
2017-03-15 04:04:38 -0400

    feat: Listing contacts with ngFor
    bind 'contacts' array to template and iterate over each item with '*ngFor'
    
    [ticket: #1]

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -25,22 +25,17 @@ var CONTACTS = [
     { id: 7, firstName: 'Harding', lastName: 'Brankovic', phone: 9025550104 },
     { id: 8, firstName: 'Galena', lastName: 'Ma', phone: 2045550176 },
     { id: 9, firstName: 'Erich', lastName: 'Ellis', phone: 2045550131 },
-    { id: 10, firstName: 'Sylvain', lastName: 'Callahan', phone: 6135550140 },
+    { id: 10, firstName: 'Sylvain', lastName: 'Callahan', phone: 6135550140 }
 ];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Contact List';
-        this.contact = {
-            id: 1,
-            firstName: 'Armin',
-            lastName: 'Eslami',
-            phone: 6132779911,
-        };
+        this.contacts = CONTACTS;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <h1>{{title}}</h1>\n        <h2>My Contacts:</h2>\n        <ul>\n            <li>\n                <!-- each contact goes here -->\n            </li>\n        </ul>\n        <h2>{{contact.firstName}} details:</h2>\n        <div>\n            <label>id: </label>{{contact.id}}\n        </div>\n        <div>\n            <label>first name: </label>\n            <input [(ngModel)]=\"contact.firstName\" placeholder=\"first name\">\n        </div>\n        <div>\n            <label>last name: </label>\n            <input value=\"{{contact.lastName}}\" placeholder=\"last name\">\n        </div>\n        <div>\n            <label>phone: </label>\n            <input value=\"{{contact.phone}}\" placeholder=\"phone\">\n        </div>\n        ",
+            template: "\n        <h1>{{title}}</h1>\n        <h2>My Contacts:</h2>\n        <ul>\n            <li *ngFor=\"let contact of contacts\">\n                <span>{{contact.id}}</span> {{contact.firstName}}\n            </li>\n        </ul>\n        <!-- <h2>{{contact.firstName}} details:</h2>\n        <div>\n            <label>id: </label>{{contact.id}}\n        </div>\n        <div>\n            <label>first name: </label>\n            <input [(ngModel)]=\"contact.firstName\" placeholder=\"first name\">\n        </div>\n        <div>\n            <label>last name: </label>\n            <input value=\"{{contact.lastName}}\" placeholder=\"last name\">\n        </div>\n        <div>\n            <label>phone: </label>\n            <input value=\"{{contact.phone}}\" placeholder=\"phone\">\n        </div> -->\n        "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,7 @@ const CONTACTS: Contact[] = [
     { id: 7, firstName: 'Harding', lastName: 'Brankovic', phone: 9025550104 },
     { id: 8, firstName: 'Galena', lastName: 'Ma', phone: 2045550176 },
     { id: 9, firstName: 'Erich', lastName: 'Ellis', phone: 2045550131 },
-    { id: 10, firstName: 'Sylvain', lastName: 'Callahan', phone: 6135550140 },
-
+    { id: 10, firstName: 'Sylvain', lastName: 'Callahan', phone: 6135550140 }
 ];
 
 @Component({
@@ -27,11 +26,11 @@ const CONTACTS: Contact[] = [
         <h1>{{title}}</h1>
         <h2>My Contacts:</h2>
         <ul>
-            <li>
-                <!-- each contact goes here -->
+            <li *ngFor="let contact of contacts">
+                <span>{{contact.id}}</span> {{contact.firstName}}
             </li>
         </ul>
-        <h2>{{contact.firstName}} details:</h2>
+        <!-- <h2>{{contact.firstName}} details:</h2>
         <div>
             <label>id: </label>{{contact.id}}
         </div>
@@ -46,15 +45,10 @@ const CONTACTS: Contact[] = [
         <div>
             <label>phone: </label>
             <input value="{{contact.phone}}" placeholder="phone">
-        </div>
-        `,
+        </div> -->
+        `
 })
 export class AppComponent {
     title = 'Contact List';
-    contact: Contact = {
-        id: 1,
-        firstName: 'Armin',
-        lastName: 'Eslami',
-        phone: 6132779911,
-    };
+    contacts = CONTACTS;
 }
