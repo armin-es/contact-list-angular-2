06791d2836e3a6645fb73102383dda453de1f290
Armin Eslami <armin.eslami@gmail.com>
2017-03-21 23:22:09 -0400

    refactor: Move 'contact detail' template to 'contact-detail.component'
    [ticket: #2]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,25 +32,6 @@ const CONTACTS: Contact[] = [
                 <span class="badge">{{contact.id}}</span> {{contact.firstName}}
             </li>
         </ul>
-
-        <div *ngIf="selectedContact">
-            <h2>{{selectedContact.firstName}} details:</h2>
-            <div>
-                <label>id: </label>{{selectedContact.id}}
-            </div>
-            <div>
-                <label>first name: </label>
-                <input [(ngModel)]="selectedContact.firstName" placeholder="first name">
-            </div>
-            <div>
-                <label>last name: </label>
-                <input value="{{selectedContact.lastName}}" placeholder="last name">
-            </div>
-            <div>
-                <label>phone: </label>
-                <input value="{{selectedContact.phone}}" placeholder="phone">
-            </div>
-        </div>
         `,
     styles: [`
         .title {
--- src/app/contact-detail.component.ts
+++ src/app/contact-detail.component.ts
@@ -2,6 +2,26 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'my-contact-detail',
+
+    template: `
+        <div *ngIf="selectedContact">
+            <h2>{{selectedContact.firstName}} details:</h2>
+            <div>
+                <label>id: </label>{{selectedContact.id}}
+            </div>
+            <div>
+                <label>first name: </label>
+                <input [(ngModel)]="selectedContact.firstName" placeholder="first name">
+            </div>
+            <div>
+                <label>last name: </label>
+                <input value="{{selectedContact.lastName}}" placeholder="last name">
+            </div>
+            <div>
+                <label>phone: </label>
+                <input value="{{selectedContact.phone}}" placeholder="phone">
+            </div>
+        </div>
 })
 
 export class ContactDetailComponent {
