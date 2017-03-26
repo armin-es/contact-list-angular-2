1552a31f43c7c7939fae31c7bb3f62aa5ea922dc
Armin Eslami <armin.eslami@gmail.com>
2017-03-25 23:49:14 -0400

    refactor: Move contact data from 'app.component.ts' to 'mock-contacts.ts'
    [ticket: #3]

---
+++ src/app/mock-contacts.ts
@@ -0,0 +1,14 @@
+import { Contact } from './contact';
+
+const CONTACTS: Contact[] = [
+    { id: 1, firstName: 'Armin', lastName: 'Eslami', phone: 6132779911 },
+    { id: 2, firstName: 'Mike', lastName: 'Pellegrino', phone: 4165550191 },
+    { id: 3, firstName: 'Rika', lastName: 'Erckens', phone: 4165550130 },
+    { id: 4, firstName: 'Luciana', lastName: 'Teke', phone: 7805550119 },
+    { id: 5, firstName: 'Nichola', lastName: 'Choudhary', phone: 7805550137 },
+    { id: 6, firstName: 'Melpomeni', lastName: 'Gerber', phone: 9025550189 },
+    { id: 7, firstName: 'Harding', lastName: 'Brankovic', phone: 9025550104 },
+    { id: 8, firstName: 'Galena', lastName: 'Ma', phone: 2045550176 },
+    { id: 9, firstName: 'Erich', lastName: 'Ellis', phone: 2045550131 },
+    { id: 10, firstName: 'Sylvain', lastName: 'Callahan', phone: 6135550140 }
+];
